c0cab5fc03d56ba0f3b6af07c34677fd82ad6a84
devxb <dlwnsdud205@naver.com>
2024-04-03 23:21:57 +0900

    build: project setup

---
+++ src/main/kotlin/org/gitanimals/render/Application.kt
@@ -0,0 +1,18 @@
+package org.gitanimals.render
+
+import org.rooftop.netx.meta.EnableSaga
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@EnableSaga
+@SpringBootApplication
+class Application {
+
+    companion object {
+        
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
