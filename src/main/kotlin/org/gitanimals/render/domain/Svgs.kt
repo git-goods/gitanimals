03820ba7a6d54a71137380c315b935e1b408072f
devxb <dlwnsdud205@naver.com>
2024-04-11 15:49:36 +0900

    fix: ClasspathResource에 classpath:를 추가한다

--- src/main/kotlin/org/gitanimals/render/domain/Svgs.kt
+++ src/main/kotlin/org/gitanimals/render/domain/Svgs.kt
@@ -3,145 +3,145 @@ package org.gitanimals.render.domain
 import org.springframework.core.io.ClassPathResource
 import java.nio.charset.Charset
 
-val whiteFieldSvg: String = ClassPathResource("persona/field/white-field.svg")
+val whiteFieldSvg: String = ClassPathResource("classpath:persona/field/white-field.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val snowyFieldSvg: String = ClassPathResource("persona/field/snowy-field.svg")
+val snowyFieldSvg: String = ClassPathResource("classpath:persona/field/snowy-field.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val gooseSvg: String = ClassPathResource("persona/animal/goose.svg")
+val gooseSvg: String = ClassPathResource("classpath:persona/animal/goose.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val gooseSunglassesSvg: String = ClassPathResource("persona/animal/goose-sunglasses.svg")
+val gooseSunglassesSvg: String = ClassPathResource("classpath:persona/animal/goose-sunglasses.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val gooseKotlinSvg: String = ClassPathResource("persona/animal/goose-kotlin.svg")
+val gooseKotlinSvg: String = ClassPathResource("classpath:persona/animal/goose-kotlin.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val gooseJavaSvg: String = ClassPathResource("persona/animal/goose-java.svg")
+val gooseJavaSvg: String = ClassPathResource("classpath:persona/animal/goose-java.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val gooseJsSvg: String = ClassPathResource("persona/animal/goose-js.svg")
+val gooseJsSvg: String = ClassPathResource("classpath:persona/animal/goose-js.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val gooseNodeSvg: String = ClassPathResource("persona/animal/goose-node.svg")
+val gooseNodeSvg: String = ClassPathResource("classpath:persona/animal/goose-node.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val gooseSwiftSvg: String = ClassPathResource("persona/animal/goose-swift.svg")
+val gooseSwiftSvg: String = ClassPathResource("classpath:persona/animal/goose-swift.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val gooseLinuxSvg: String = ClassPathResource("persona/animal/goose-linux.svg")
+val gooseLinuxSvg: String = ClassPathResource("classpath:persona/animal/goose-linux.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val gooseSpringSvg: String = ClassPathResource("persona/animal/goose-spring.svg")
+val gooseSpringSvg: String = ClassPathResource("classpath:persona/animal/goose-spring.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val littleChickSvg: String = ClassPathResource("persona/animal/little-chick.svg")
+val littleChickSvg: String = ClassPathResource("classpath:persona/animal/little-chick.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val littleChickSunglassesSvg: String = ClassPathResource("persona/animal/little-chick-sunglasses.svg")
+val littleChickSunglassesSvg: String = ClassPathResource("classpath:persona/animal/little-chick-sunglasses.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val littleChickKotlinSvg: String = ClassPathResource("persona/animal/little-chick-kotlin.svg")
+val littleChickKotlinSvg: String = ClassPathResource("classpath:persona/animal/little-chick-kotlin.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val littleChickJavaSvg: String = ClassPathResource("persona/animal/little-chick-java.svg")
+val littleChickJavaSvg: String = ClassPathResource("classpath:persona/animal/little-chick-java.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val littleChickJsSvg: String = ClassPathResource("persona/animal/little-chick-js.svg")
+val littleChickJsSvg: String = ClassPathResource("classpath:persona/animal/little-chick-js.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val littleChickNodeSvg: String = ClassPathResource("persona/animal/little-chick-node.svg")
+val littleChickNodeSvg: String = ClassPathResource("classpath:persona/animal/little-chick-node.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val littleChickSwiftSvg: String = ClassPathResource("persona/animal/little-chick-swift.svg")
+val littleChickSwiftSvg: String = ClassPathResource("classpath:persona/animal/little-chick-swift.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val littleChickLinuxSvg: String = ClassPathResource("persona/animal/little-chick-linux.svg")
+val littleChickLinuxSvg: String = ClassPathResource("classpath:persona/animal/little-chick-linux.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val littleChickSpringSvg: String = ClassPathResource("persona/animal/little-chick-spring.svg")
+val littleChickSpringSvg: String = ClassPathResource("classpath:persona/animal/little-chick-spring.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val penguinSvg: String = ClassPathResource("persona/animal/penguin.svg")
+val penguinSvg: String = ClassPathResource("classpath:persona/animal/penguin.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val penguinSunglassesSvg: String = ClassPathResource("persona/animal/penguin-sunglasses.svg")
+val penguinSunglassesSvg: String = ClassPathResource("classpath:persona/animal/penguin-sunglasses.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val penguinKotlinSvg: String = ClassPathResource("persona/animal/penguin-kotlin.svg")
+val penguinKotlinSvg: String = ClassPathResource("classpath:persona/animal/penguin-kotlin.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val penguinJavaSvg: String = ClassPathResource("persona/animal/penguin-java.svg")
+val penguinJavaSvg: String = ClassPathResource("classpath:persona/animal/penguin-java.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val penguinJsSvg: String = ClassPathResource("persona/animal/penguin-js.svg")
+val penguinJsSvg: String = ClassPathResource("classpath:persona/animal/penguin-js.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val penguinNodeSvg: String = ClassPathResource("persona/animal/penguin-node.svg")
+val penguinNodeSvg: String = ClassPathResource("classpath:persona/animal/penguin-node.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val penguinSwiftSvg: String = ClassPathResource("persona/animal/penguin-swift.svg")
+val penguinSwiftSvg: String = ClassPathResource("classpath:persona/animal/penguin-swift.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val penguinLinuxSvg: String = ClassPathResource("persona/animal/penguin-linux.svg")
+val penguinLinuxSvg: String = ClassPathResource("classpath:persona/animal/penguin-linux.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val penguinSpringSvg: String = ClassPathResource("persona/animal/penguin-spring.svg")
+val penguinSpringSvg: String = ClassPathResource("classpath:persona/animal/penguin-spring.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val pigSvg: String = ClassPathResource("persona/animal/pig.svg")
+val pigSvg: String = ClassPathResource("classpath:persona/animal/pig.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val pigSunglassesSvg: String = ClassPathResource("persona/animal/pig-sunglasses.svg")
+val pigSunglassesSvg: String = ClassPathResource("classpath:persona/animal/pig-sunglasses.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val pigKotlinSvg: String = ClassPathResource("persona/animal/pig-kotlin.svg")
+val pigKotlinSvg: String = ClassPathResource("classpath:persona/animal/pig-kotlin.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val pigJavaSvg: String = ClassPathResource("persona/animal/pig-java.svg")
+val pigJavaSvg: String = ClassPathResource("classpath:persona/animal/pig-java.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val pigJsSvg: String = ClassPathResource("persona/animal/pig-js.svg")
+val pigJsSvg: String = ClassPathResource("classpath:persona/animal/pig-js.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val pigNodeSvg: String = ClassPathResource("persona/animal/pig-node.svg")
+val pigNodeSvg: String = ClassPathResource("classpath:persona/animal/pig-node.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val pigSwiftSvg: String = ClassPathResource("persona/animal/pig-swift.svg")
+val pigSwiftSvg: String = ClassPathResource("classpath:persona/animal/pig-swift.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val pigLinuxSvg: String = ClassPathResource("persona/animal/pig-linux.svg")
+val pigLinuxSvg: String = ClassPathResource("classpath:persona/animal/pig-linux.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val pigSpringSvg: String = ClassPathResource("persona/animal/pig-spring.svg")
+val pigSpringSvg: String = ClassPathResource("classpath:persona/animal/pig-spring.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val slimeRedSvg: String = ClassPathResource("persona/animal/slime-red.svg")
+val slimeRedSvg: String = ClassPathResource("classpath:persona/animal/slime-red.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val slimeRedKotlinSvg: String = ClassPathResource("persona/animal/slime-red-kotlin.svg")
+val slimeRedKotlinSvg: String = ClassPathResource("classpath:persona/animal/slime-red-kotlin.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val slimeRedJavaSvg: String = ClassPathResource("persona/animal/slime-red-java.svg")
+val slimeRedJavaSvg: String = ClassPathResource("classpath:persona/animal/slime-red-java.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val slimeRedJsSvg: String = ClassPathResource("persona/animal/slime-red-js.svg")
+val slimeRedJsSvg: String = ClassPathResource("classpath:persona/animal/slime-red-js.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val slimeRedNodeSvg: String = ClassPathResource("persona/animal/slime-red-node.svg")
+val slimeRedNodeSvg: String = ClassPathResource("classpath:persona/animal/slime-red-node.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val slimeRedSwiftSvg: String = ClassPathResource("persona/animal/slime-red-swift.svg")
+val slimeRedSwiftSvg: String = ClassPathResource("classpath:persona/animal/slime-red-swift.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val slimeRedLinuxSvg: String = ClassPathResource("persona/animal/slime-red-linux.svg")
+val slimeRedLinuxSvg: String = ClassPathResource("classpath:persona/animal/slime-red-linux.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val slimeBlueSvg: String = ClassPathResource("persona/animal/slime-blue.svg")
+val slimeBlueSvg: String = ClassPathResource("classpath:persona/animal/slime-blue.svg")
     .getContentAsString(Charset.defaultCharset())
 
-val slimeGreenSvg: String = ClassPathResource("persona/animal/slime-green.svg")
+val slimeGreenSvg: String = ClassPathResource("classpath:persona/animal/slime-green.svg")
     .getContentAsString(Charset.defaultCharset())
 
 val largeTextSvgs = lazy {
@@ -161,7 +161,7 @@ val largeTextSvgs = lazy {
         map[i.toString()] = ClassPathResource(path)
             .getContentAsString(Charset.defaultCharset())
     }
-    map["-"] = ClassPathResource("persona/text/large/hyphens.svg")
+    map["-"] = ClassPathResource("classpath:persona/text/large/hyphens.svg")
         .getContentAsString(Charset.defaultCharset())
     map
 }.value
