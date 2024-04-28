be582881c1264b4e78abc3803a9746bdc048fc88
devxb <dlwnsdud205@naver.com>
2024-04-28 12:30:45 +0900

    feat: InternalApi interceptor를 정의한다

---
+++ src/main/kotlin/org/gitanimals/render/controller/interceptor/InterceptorConfigurer.kt
@@ -0,0 +1,22 @@
+package org.gitanimals.render.controller.interceptor
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class InterceptorConfigurer(
+    @Value("\${white.ip}") private val whiteIps: List<String>
+) : WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(internalApiInterceptor())
+            .addPathPatterns("/internals/**")
+            .excludePathPatterns()
+    }
+
+    @Bean
+    fun internalApiInterceptor(): InternalApiInterceptor = InternalApiInterceptor(whiteIps)
+}
