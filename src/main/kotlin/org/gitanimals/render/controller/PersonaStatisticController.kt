e8b1707977e0c22b34d4e444e82e77937737727c
devxb <develxb@gmail.com>
2024-07-14 11:28:05 +0900

    refactor: persona, user statistic controller를 분리한다

--- src/main/kotlin/org/gitanimals/render/controller/PersonaStatisticController.kt
+++ src/main/kotlin/org/gitanimals/render/controller/PersonaStatisticController.kt
@@ -1,21 +1,14 @@
 package org.gitanimals.render.controller
 
 import org.gitanimals.render.controller.response.TotalPersonaResponse
-import org.gitanimals.render.controller.response.TotalUserResponse
 import org.gitanimals.render.domain.PersonaStatisticService
-import org.gitanimals.render.domain.UserStatisticService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class StatisticController(
-    private val userStatisticService: UserStatisticService,
+class PersonaStatisticController(
     private val personaStatisticService: PersonaStatisticService,
 ) {
-
-    @GetMapping("/users/statistics/total")
-    fun totalUsers(): TotalUserResponse =
-        TotalUserResponse.from(userStatisticService.getTotalUserCount())
 
     @GetMapping("/personas/statistics/total")
     fun totalAdaptedPersonas(): TotalPersonaResponse =
