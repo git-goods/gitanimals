ce1864e7b85bffa40516c31aa75f4cad701f7530
devxb <develxb@gmail.com>
2025-01-18 01:47:31 +0900

    refactor: 모든 api 요청에 traceId를 포함한다

--- src/main/kotlin/org/gitanimals/render/infra/RestIdentityApi.kt
+++ src/main/kotlin/org/gitanimals/render/infra/RestIdentityApi.kt
@@ -1,7 +1,9 @@
 package org.gitanimals.render.infra
 
 import org.gitanimals.core.AUTHORIZATION_EXCEPTION
+import org.gitanimals.core.filter.MDCFilter.Companion.TRACE_ID
 import org.gitanimals.render.app.IdentityApi
+import org.slf4j.MDC
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
@@ -15,6 +17,7 @@ class RestIdentityApi : IdentityApi {
         return restClient.get()
             .uri("/users")
             .header(HttpHeaders.AUTHORIZATION, token)
+            .header(TRACE_ID, MDC.get(TRACE_ID))
             .exchange { _, response ->
                 runCatching {
                     response.bodyTo(IdentityApi.UserResponse::class.java)
