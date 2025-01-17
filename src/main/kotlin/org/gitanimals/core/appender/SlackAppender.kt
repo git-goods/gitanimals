7c813f04c4ed0d7b4a504c60a3dc7abed46a7a01
devxb <develxb@gmail.com>
2025-01-17 23:10:26 +0900

    feat: efk, slack 로깅 환경을 구성한다

---
+++ src/main/kotlin/org/gitanimals/core/appender/SlackAppender.kt
@@ -0,0 +1,44 @@
+package org.gitanimals.core.appender
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.AppenderBase
+import com.slack.api.Slack
+import com.slack.api.methods.MethodsClient
+import com.slack.api.methods.request.chat.ChatPostMessageRequest
+
+class SlackAppender : AppenderBase<ILoggingEvent>() {
+
+    lateinit var slack: MethodsClient
+
+    private val errorChannel = "C080GR85WM9"
+    private val warnChannel = "C08977RL38C"
+
+    override fun append(eventObject: ILoggingEvent) {
+        runCatching {
+            val channel = when (eventObject.level) {
+                Level.WARN -> warnChannel
+                Level.ERROR -> errorChannel
+                else -> NOT_LOGGING
+            }
+
+            if (channel == NOT_LOGGING) {
+                return
+            }
+
+            val request: ChatPostMessageRequest = ChatPostMessageRequest.builder()
+                .channel(channel)
+                .text(eventObject.formattedMessage)
+                .build();
+            slack.chatPostMessage(request)
+        }
+    }
+
+    fun setToken(token: String) {
+        this.slack = Slack.getInstance().methods(token)
+    }
+
+    companion object {
+        private const val NOT_LOGGING = "NOT LOGGING"
+    }
+}
