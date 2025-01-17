f60752eace14725d96751ba5adeb7e5a1ef2247e
devxb <develxb@gmail.com>
2025-01-18 01:20:56 +0900

    feat: MDCFilter에서 수행시간과 url을 함께 남긴다

--- src/main/kotlin/org/gitanimals/core/filter/MDCFilter.kt
+++ src/main/kotlin/org/gitanimals/core/filter/MDCFilter.kt
@@ -4,9 +4,11 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.gitanimals.core.IdGenerator
+import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
+import kotlin.system.measureTimeMillis
 
 @Component
 class MDCFilter : OncePerRequestFilter() {
@@ -16,17 +18,30 @@ class MDCFilter : OncePerRequestFilter() {
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
+        val logger = LoggerFactory.getLogger("Request logger")
+
         runCatching {
             val traceId: String = request.getHeader("traceId") ?: IdGenerator.generate().toString()
 
             MDC.put(TRACE_ID, traceId)
-            filterChain.doFilter(request, response)
+            MDC.put(PATH, request.requestURI)
+
+            val elapsedTime = measureTimeMillis {
+                filterChain.doFilter(request, response)
+            }
+            MDC.put(ELAPSED_TIME, elapsedTime.toString())
+        }.onSuccess {
+            logger.info("Request Success with status ${response.status}")
         }.also {
             MDC.remove(TRACE_ID)
+            MDC.remove(ELAPSED_TIME)
+            MDC.remove(PATH)
         }
     }
 
     companion object {
         const val TRACE_ID = "traceId"
+        const val ELAPSED_TIME = "elapsedTime"
+        const val PATH = "path"
     }
 }
