e1092a63814912d5981c91ca3eafa3030030deac
devxb <develxb@gmail.com>
2025-03-03 11:19:18 +0900

    refactor: pub/sub listener logging 수정

--- src/main/kotlin/org/gitanimals/rank/infra/RankRedisEventSubscriber.kt
+++ src/main/kotlin/org/gitanimals/rank/infra/RankRedisEventSubscriber.kt
@@ -30,7 +30,7 @@ class RankRedisEventSubscriber(
                 ChannelTopic(RedisPubSubChannel.GUILD_CONTRIBUTION_UPDATED),
             )
             this.setErrorHandler {
-                logger.error("Fail to listen message ${it.stackTrace}")
+                logger.error("Fail to listen message ${it.message}", it)
             }
         }
     }
