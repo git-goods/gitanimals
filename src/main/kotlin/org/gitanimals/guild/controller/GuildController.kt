10c1bbee449cbcd4277d53069dada505378b1748
devxb <develxb@gmail.com>
2025-03-26 22:44:38 +0900

    feat: Error를 내는 Controller를 추가한다

--- src/main/kotlin/org/gitanimals/guild/controller/GuildController.kt
+++ src/main/kotlin/org/gitanimals/guild/controller/GuildController.kt
@@ -33,6 +33,13 @@ class GuildController(
     private val denyJoinGuildFacade: DenyJoinGuildFacade,
 ) {
 
+    @GetMapping("/error")
+    fun doError() {
+        throw IllegalStateException("error!", throwError())
+    }
+
+    fun throwError() = Exception("error error")
+
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/guilds")
     fun createGuild(
