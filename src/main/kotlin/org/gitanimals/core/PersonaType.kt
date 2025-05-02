c26b76345ade95688fd4964938b6a6ddae12518d
devxb <develxb@gmail.com>
2025-05-02 20:57:33 +0900

    feat: SHIBA 🐾

--- src/main/kotlin/org/gitanimals/core/PersonaType.kt
+++ src/main/kotlin/org/gitanimals/core/PersonaType.kt
@@ -2082,7 +2082,28 @@ enum class PersonaType(val weight: Double, private var dropRate: String? = null)
         override fun act(id: Long, flippedWidth: Double): String =
             StringBuilder().moveRandomly("unicorn", id, 40, "180s", 5, 12.0)
                 .toString()
-    }
+    },
+
+    SHIBA(0.3) {
+        override fun loadSvg(name: String, animationId: Long, level: Long, mode: Mode): String {
+            return shibaSvg.replace("*{act}", act(animationId))
+                .replace("*{id}", animationId.toString())
+                .replace("*{level}", level.toSvg(14.0, 2.0))
+                .replace(
+                    "*{levelx}",
+                    (-5 + (-1 * (level.toString().length))).toString()
+                )
+                .replace("*{username}", name.toSvg(14.0, 25.0))
+                .replace(
+                    "*{usernamex}",
+                    (20 + (-3 * name.length)).toString()
+                )
+        }
+
+        override fun act(id: Long, flippedWidth: Double): String =
+            StringBuilder().moveRandomly("shiba", id, 20, "180s", 5, 18.0)
+                .toString()
+    },
     ;
 
     init {
