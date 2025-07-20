78e2106b8c949ffe96cbee29e9b3a2667a2a3863
devxb <develxb@gmail.com>
2025-07-20 19:19:04 +0900

    fix: 포인트 컷 매핑 버그를 수정한다

--- src/main/kotlin/org/gitanimals/supports/schedule/SchedulerTraceIdAspect.kt
+++ src/main/kotlin/org/gitanimals/supports/schedule/SchedulerTraceIdAspect.kt
@@ -7,7 +7,6 @@ import org.gitanimals.core.IdGenerator
 import org.gitanimals.core.filter.MDCFilter.Companion.TRACE_ID
 import org.slf4j.MDC
 import org.springframework.core.annotation.Order
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Aspect
@@ -15,8 +14,8 @@ import org.springframework.stereotype.Component
 @Order(value = Int.MIN_VALUE)
 class SchedulerTraceIdAspect {
 
-    @Around("@annotation(scheduled)")
-    fun putMdcFilter(joinPoint: ProceedingJoinPoint, scheduled: Scheduled): Any? {
+    @Around("@annotation(org.springframework.scheduling.annotation.Scheduled)")
+    fun putMdcFilter(joinPoint: ProceedingJoinPoint): Any? {
         return runCatching {
             MDC.put(TRACE_ID, IdGenerator.generate().toString())
             joinPoint.proceed()
