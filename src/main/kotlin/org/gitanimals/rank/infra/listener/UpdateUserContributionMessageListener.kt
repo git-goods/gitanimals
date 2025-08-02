f3398d6cd68458f26f73a8209374028b2d6164f4
devxb <develxb@gmail.com>
2025-08-02 15:39:03 +0900

    refactor: rank 도메인 listener위치를 수정한다

--- src/main/kotlin/org/gitanimals/rank/infra/listener/UpdateUserContributionMessageListener.kt
+++ src/main/kotlin/org/gitanimals/rank/infra/listener/UpdateUserContributionMessageListener.kt
@@ -1,4 +1,4 @@
-package org.gitanimals.rank.infra
+package org.gitanimals.rank.infra.listener
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.gitanimals.core.redis.TraceableMessageListener
