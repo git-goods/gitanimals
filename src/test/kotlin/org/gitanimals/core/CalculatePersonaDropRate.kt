bfe860e1f9768a16d271f68301ab8c6ffbc9d911
devxb <develxb@gmail.com>
2025-09-27 17:38:40 +0900

    feat: 진화 펫 등장 확률 모니터링 로직작성

---
+++ src/test/kotlin/org/gitanimals/core/CalculatePersonaDropRate.kt
@@ -0,0 +1,24 @@
+package org.gitanimals.core
+
+import io.kotest.core.spec.style.StringSpec
+
+internal class CalculatePersonaDropRate : StringSpec({
+
+    "특정 진화 펫 등장확률 테스트" {
+        val dropCount = 10000
+        val expectedPersona = PersonaType.LITTLE_CHICK_EGG_ON_HAT
+
+        val results = mutableMapOf<PersonaType, Int>()
+        repeat(dropCount) {
+            val evolutionResult = expectedPersona.randomEvolution()
+
+            results[evolutionResult] = (results[evolutionResult]?.plus(1)) ?: 1
+        }
+
+        println("--- 모수: $dropCount ---")
+        results.forEach { (key, value) ->
+            val dropPercentage = (value.toDouble() / dropCount.toDouble()) * 100
+            println("$key -> $dropPercentage% ($value)")
+        }
+    }
+})
